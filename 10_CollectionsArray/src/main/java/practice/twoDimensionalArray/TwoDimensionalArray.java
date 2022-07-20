f1da582d8e42816982e105189f5b192827cfe3e8
Roman Blinov <romanblinov1613@gmail.com>
2022-07-20 12:33:08 +0300

    add practical work 10.5 task 3 skillbox

---
+++ 10_CollectionsArray/src/main/java/practice/twoDimensionalArray/TwoDimensionalArray.java
@@ -0,0 +1,28 @@
+package practice.twoDimensionalArray;
+
+public class TwoDimensionalArray {
+
+    public static final char SYMBOL = 'X';
+
+    public static char[][] getTwoDimensionalArray(int size) {
+
+        //TODO: Написать метод, который создаст двумерный массив char заданного размера.
+        // массив должен содержать символ SYMBOL по диагоналям, пример для size = 3
+        // [X,  , X]
+        // [ , X,  ]
+        // [X,  , X]
+
+        char[][] ggg = new char[size][size];
+
+        for (int i = 0; i < ggg.length; i++) {
+            for (int j = 0; j < ggg.length; j++) {
+                if (i == j || i + j == (size - 1)) {
+                    ggg[i][j] = SYMBOL;
+                } else {
+                    ggg[i][j] = ' ';
+                }
+            }
+        }
+        return ggg;
+    }
+}
