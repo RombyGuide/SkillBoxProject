6e869a59264e6f6169499697ec8a0739ea9ed950
Roman Blinov <romanblinov1613@gmail.com>
2022-07-24 10:29:37 +0300

    add practical work 13.12 task 2 airport skillbox

---
+++ 13_AdvancedOOPFeatures/practice/LambdaExpressions-Airport/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import com.skillbox.airport.Airport;
+import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+        //Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
+
+        final int TWO_HOURS = 7_200_000;
+        return airport.getTerminals().stream()
+                .map(Terminal::getFlights)
+                .flatMap(List::stream)
+                .filter(flight -> flight.getType() == Flight.Type.DEPARTURE
+                        && flight.getDate().getTime() > System.currentTimeMillis()
+                        && flight.getDate().getTime() < System.currentTimeMillis() + TWO_HOURS)
+                .collect(Collectors.toList());
+    }
+}
