2fc1362cabd9bdd014608a001134064f5b640dcb
Roman Blinov <romanblinov1613@gmail.com>
2022-07-29 19:19:42 +0300

    add independent practical work

---
+++ 15_FilesAndNetwork/DataCollector/src/main/java/CopyFileInFolder.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class CopyFileInFolder {
+    public static void main(final String[] args) throws IOException {
+
+        AtomicLong directoryNumber = new AtomicLong(0);
+        Path start = Paths.get("FilesAndNetwork/DataCollector/data");
+
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+                String fileName = file.getFileName().toString();
+                String newFileName;
+                int index = fileName.lastIndexOf('.');
+                if (index == -1)
+                    newFileName = fileName;
+                else
+                    newFileName = directoryNumber.get() + fileName.substring(index);
+
+                Path target = Paths.get(file.getParent().toString(), newFileName);
+                Files.copy(file, target, StandardCopyOption.REPLACE_EXISTING);
+
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                directoryNumber.incrementAndGet();
+                return super.preVisitDirectory(dir, attrs);
+            }
+        });
+    }
+}
