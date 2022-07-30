9fda065acaabb90f1c1272514fe51673363ad970
Roman Blinov <romanblinov1613@gmail.com>
2022-07-30 16:44:38 +0300

    add practical code skillbox to the module 15 Files and networks

---
+++ 15_FilesAndNetwork/DataCollector/src/main/java/ParserFile.java
@@ -0,0 +1,34 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ParserFile {
+    public static void main(String[] args) {
+        String htmlFile = parseFile("04_ObjectsAndMethods/data/code.html");
+
+        Document doc = Jsoup.parse(htmlFile);
+        Elements elements = doc.select("a.menu__link");
+        elements.forEach(element -> System.out.println(element.text()));
+
+        System.out.println(htmlFile);
+    }
+
+    public static String parseFile(String path) {
+        StringBuilder builder = new StringBuilder();
+
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(Paths.get(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        lines.forEach(line -> builder.append(line + "\n"));
+
+        return builder.toString();
+    }
+}
