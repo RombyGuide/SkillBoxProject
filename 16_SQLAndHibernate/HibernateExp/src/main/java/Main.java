c972a32245ffa2285becd1c2db864e744b08640c
Roman Blinov <romanblinov1613@gmail.com>
2022-08-26 21:16:43 +0300

    add changed in Main

--- 16_SQLAndHibernate/HibernateExp/src/main/java/Main.java
+++ 16_SQLAndHibernate/HibernateExp/src/main/java/Main.java
@@ -6,12 +6,10 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                        .configure("hibernate.cfg.xml").build();
+                .configure("hibernate.cfg.xml").build();
         Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
         SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
@@ -26,12 +24,17 @@ public class Main {
 
         Transaction transaction = session.beginTransaction();
 
-        Course course = session.get(Course.class, 5);
-        List<Student> students = course.getStudents();
-        students.forEach(student -> System.out.println(student.getName()));
+        Course course = session.get(Course.class, 1);
+        Student student = session.get(Student.class, 2);
+//        List<Student> students = course.getStudents();
+//        students.forEach(student -> System.out.println(student.getName()));
+
+
+        Subscription subscription = session.get(Subscription.class,
+                new KeySubscription(student.getId(), course.getId()));
+        System.out.println(subscription.getStudent().getName());
 
         transaction.commit();
-
         sessionFactory.close();
     }
 }
