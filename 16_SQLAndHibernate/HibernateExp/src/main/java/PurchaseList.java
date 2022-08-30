01637f24eb221bc40b82cf7e418e72601bd6e3bd
Roman Blinov <romanblinov1613@gmail.com>
2022-08-30 18:58:31 +0300

    add an interuction class to a new table

---
+++ 16_SQLAndHibernate/HibernateExp/src/main/java/PurchaseList.java
@@ -0,0 +1,64 @@
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "purchaselist")
+public class PurchaseList {
+
+    @EmbeddedId
+    private KeyPurchaseList key;
+
+    @Column(name = "student_name", insertable = false, updatable = false)
+    private String studentName;
+
+    @Column(name = "course_name", insertable = false, updatable = false)
+    private String courseName;
+
+    private Integer price;
+
+    @Column(name = "subscription_date")
+    private LocalDateTime subscriptionDate;
+
+    public KeyPurchaseList getKey() {
+        return key;
+    }
+
+    public void setKey(KeyPurchaseList key) {
+        this.key = key;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getSubscriptionDate() {
+        return subscriptionDate;
+    }
+
+    public void setSubscriptionDate(LocalDateTime subscriptionDate) {
+        this.subscriptionDate = subscriptionDate;
+    }
+}
