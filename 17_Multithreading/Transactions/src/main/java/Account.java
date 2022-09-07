88ffabd0c598c3b6511db0d09248f7bc9843fe8d
Roman Blinov <romanblinov1613@gmail.com>
2022-09-07 19:24:26 +0300

    add practical work 17.13 №2 skillbox

---
+++ 17_Multithreading/Transactions/src/main/java/Account.java
@@ -0,0 +1,50 @@
+public class Account {
+
+    private long money;
+    private String accNumber;
+    private volatile boolean blockingAccounts;
+
+    public Account() {
+        money = 0;
+        accNumber = "";
+        blockingAccounts = false;
+    }
+
+    public Account(long money, String accNumber) {
+        this.money = money;
+        this.accNumber = accNumber;
+        blockingAccounts = false;
+    }
+
+    public boolean isBlockingAccounts() {
+        return blockingAccounts;
+    }
+
+    public void setBlockingAccounts() {
+        blockingAccounts = true;
+    }
+
+    public long getMoney() {
+        return money;
+    }
+
+    public void setMoney(long money) {
+        this.money = money;
+    }
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public void setAccNumber(String accNumber) {
+        this.accNumber = accNumber;
+    }
+
+    public void addMoney(long amount) {
+        money += amount;
+    }
+
+    public void reduceMoney(long amount) {
+        money -= amount;
+    }
+}
---
+++ 17_Multithreading/Transactions/src/main/java/Bank.java
@@ -0,0 +1,86 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Bank {
+
+    private Map<String, Account> accounts = new HashMap<>();
+    private final Random random = new Random();
+    private int numberTransaction = 1;
+
+    public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
+            throws InterruptedException {
+        Thread.sleep(1000);
+        return random.nextBoolean();
+    }
+
+    private void standardTransfer(String fromAccountNum, String toAccountNum, long amount) {
+        int fromAccount = Integer.parseInt(accounts.get(fromAccountNum).getAccNumber());
+        int toAccount = Integer.parseInt(accounts.get(toAccountNum).getAccNumber());
+        if (fromAccount < toAccount) {
+            synchronized (accounts.get(fromAccountNum)) {
+                synchronized (accounts.get(toAccountNum)) {
+                    accounts.get(fromAccountNum).reduceMoney(amount);
+                    accounts.get(toAccountNum).addMoney(amount);
+                }
+            }
+        } else {
+            synchronized (accounts.get(toAccountNum)) {
+                synchronized (accounts.get(fromAccountNum)) {
+                    accounts.get(fromAccountNum).reduceMoney(amount);
+                    accounts.get(toAccountNum).addMoney(amount);
+                }
+            }
+        }
+        System.out.println("Транзакция №" + numberTransaction++ + " прошла успешно.\n" +
+                "Баланс отправителя " + fromAccountNum + " - " + getBalance(fromAccountNum) + " руб.\n" +
+                "Баланс получателя " + toAccountNum + " - " + getBalance(toAccountNum) + " руб.\n");
+    }
+
+    /**
+     * Метод переводит деньги между счетами. Если сумма транзакции > 50000,
+     * то после совершения транзакции, она отправляется на проверку Службе Безопасности – вызывается
+     * метод isFraud. Если возвращается true, то делается блокировка счетов (как – на ваше
+     * усмотрение)
+     */
+    public void transfer(String fromAccountNum, String toAccountNum, long amount) {
+        if (accounts.get(fromAccountNum).isBlockingAccounts()) {
+            System.out.println("Транзакция №" + numberTransaction++ +  " невозможна, счет отправителя: " +
+                    fromAccountNum + " заблокирован!\n");
+        } else if (accounts.get(toAccountNum).isBlockingAccounts()) {
+            System.out.println("Транзакция №" + numberTransaction++ +  " невозможна, счет получателя: " +
+                    toAccountNum + " заблокирован!\n");
+        } else if (getBalance(fromAccountNum) < amount) {
+            System.out.println("Транзакция №" + numberTransaction++ + " невозможна, недостаточно средств на счету " +
+                    fromAccountNum + ", остаток: " + getBalance(fromAccountNum) + " руб.\n");
+        } else if (amount > 50000) {
+            try {
+                if (isFraud(fromAccountNum, toAccountNum, amount)) {
+                    accounts.get(fromAccountNum).setBlockingAccounts();
+                    accounts.get(toAccountNum).setBlockingAccounts();
+                    System.out.println("Подозрительная транзакция! Транзакция № " + numberTransaction++ +
+                            ", оба счета заблокированы: " + fromAccountNum + ", " + toAccountNum + "\n");
+                }
+                else {
+                    standardTransfer(fromAccountNum, toAccountNum, amount);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            standardTransfer(fromAccountNum, toAccountNum, amount);
+        }
+    }
+
+    /**
+     * Метод getBalance() возвращает остаток на счёте.
+     */
+    public long getBalance(String accountNum) {
+        return accounts.get(accountNum).getMoney();
+    }
+
+    public void addAccount(String num, Account account) {
+        accounts.put(num, account);
+    }
+}
