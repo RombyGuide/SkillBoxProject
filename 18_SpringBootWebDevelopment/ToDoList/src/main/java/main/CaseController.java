015e5d337ea8a44a3af5b4fd43b19e168c7c9c81
Roman Blinov <romanblinov1613@gmail.com>
2022-10-05 19:01:43 +0300

    add practical work 19.14 №3 skillbox

--- 18_SpringBootWebDevelopment/ToDoList/src/main/java/main/CaseController.java
+++ 18_SpringBootWebDevelopment/ToDoList/src/main/java/main/CaseController.java
@@ -1,69 +1,82 @@
 package main;
 
+import main.model.Case;
+import main.model.CaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import main.model.Case;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-public class TaskController {
+public class CaseController {
+
+    @Autowired
+    private CaseRepository caseRepository;
 
     // метод возвращающий список дел
     @GetMapping("/cases/")
     public List<Case> list() {
-        return Storage.getAllCase();
+        Iterable<Case> caseIterable = caseRepository.findAll();
+        ArrayList<Case> caseArrayList = new ArrayList<>();
+        caseIterable.forEach(caseArrayList::add);
+        return caseArrayList;
     }
 
     // метод добавления дела
     @PostMapping("/cases/")
     public int add(Case task) {
-        return Storage.addCase(task);
+        Case newCase = caseRepository.save(task);
+        return newCase.getId();
     }
 
     // метод получения дела по id
     @GetMapping("/cases/{id}")
     public ResponseEntity get(@PathVariable int id) {
-        Case aCase = Storage.getCase(id);
-        if (aCase == null) {
+        Optional<Case> caseOptional = caseRepository.findById(id);
+        if (!caseOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        return new ResponseEntity(aCase, HttpStatus.OK);
+        return new ResponseEntity(caseOptional.get(), HttpStatus.OK);
     }
 
     // метод обновления дела по id
     @PutMapping("/cases/{id}")
     public ResponseEntity put(@PathVariable int id, Case updatedCase) {
-        Case aCase = Storage.getCase(id);
-        if (aCase == null) {
+        Optional<Case> caseOptional = caseRepository.findById(id);
+        if (!caseOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         } else {
-            Storage.updateCase(id, updatedCase);
-            return new ResponseEntity(aCase, HttpStatus.OK);
+            updatedCase.setId(id);
+            caseRepository.save(updatedCase);
+            return new ResponseEntity(HttpStatus.OK);
         }
     }
 
     // метод удаления списка дел
     @PostMapping("/cases/delete/")
     public ResponseEntity delete(Case aCase) {
-        if (aCase == null) {
+        ArrayList<Case> caseArrayList = (ArrayList<Case>) caseRepository.findAll();
+        if (caseArrayList.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         } else {
-            Storage.deleteCases();
-            return ResponseEntity.status(HttpStatus.OK).body(null);
+            caseRepository.deleteAll();
+            return new ResponseEntity(HttpStatus.OK);
         }
     }
 
     // метод удаления дела по id
     @DeleteMapping("/cases/{id}")
     public ResponseEntity deleteCase(@PathVariable int id) {
-        Case aCase = Storage.getCase(id);
-        if (aCase == null) {
+        Optional<Case> caseOptional = caseRepository.findById(id);
+        if (!caseOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         } else  {
-            Storage.deleteCase(id);
-            return new ResponseEntity(aCase, HttpStatus.OK);
+            caseRepository.deleteById(id);
+            return new ResponseEntity(HttpStatus.OK);
         }
     }
 }
